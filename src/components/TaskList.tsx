f0716b64acfa19e61b49de1568c343fed708b524
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-05 05:00:26 +0100

    some changes

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -51,7 +51,7 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>My Tasks</h2>
 
         <div className="input-group">
           <input 
