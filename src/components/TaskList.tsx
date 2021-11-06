5166ef24cb661bf62fb1d79449dffb69365583bc
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-06 18:49:05 +0100

    changes

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -56,7 +56,7 @@ export function TaskList() {
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Add New Task" 
+            placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
